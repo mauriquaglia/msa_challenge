7f1d8a680da94b131b115f6458f6e189bce27276
mauriquaglia <mauriquaglia@gmail.com>
2022-08-18 15:29:19 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -13,6 +13,7 @@ from bson.json_util import dumps, loads
 import math
 import datetime
 import os
+import logging
 
 class PyObjectId(ObjectId):
     @classmethod
@@ -83,10 +84,6 @@ def create_obj(name, category):
 
 app = FastAPI()
 
-#URL = "http://httpbin.org/uuid"
-#URL = "https://wger.de/api/v2/exerciseinfo/?limit=500"
-
-
 
 async def request(client, URL):
     response = await client.get(URL)
@@ -116,84 +113,105 @@ db = client['msa']
 
 @app.get("/")
 async def root():
-    start = time()
-    count = await task()
-    count = count[0]['count']
-    counter = 0
-    result = await task("https://wger.de/api/v2/exerciseinfo/?limit="+str(count))
+    try:
+        start = time()
+        count = await task()
+        count = count[0]['count']
+        counter = 0
+        result = await task("https://wger.de/api/v2/exerciseinfo/?limit="+str(count))
 
-    for x in result[0]['results']:
-        if x['category']['name'] == 'Abs': 
-            counter=counter+1
-            append(Exercises2(name= x['name'], 
-            description= x['description'], 
-            category=x['category']['name'], 
-            id_category=x['category']['id'] ,
-            muscles=x['muscles'],
-            equipment=x['equipment']).dict())
-   
-    result = append('', True)
-    #db.exercises.insert_many(result)
-    return {"message": time() - start, "count": counter}
+        for x in result[0]['results']:
+            if x['category']['name'] == 'Abs': 
+                counter=counter+1
+                append(Exercises2(name= x['name'], 
+                description= x['description'], 
+                category=x['category']['name'], 
+                id_category=x['category']['id'] ,
+                muscles=x['muscles'],
+                equipment=x['equipment']).dict())
+    
+        result = append('', True)
+        #db.exercises.insert_many(result)
+        logging.info('GET /')
+        return {"message": time() - start, "count": counter}
+    except Exception as ex:
+        logging.exception(ex)
+        raise
 
 @app.get("/import_category/{name}")
 async def import_category(name: str):
-    start = time()
-    count = await task()
-    count = count[0]['count']
-    counter = 0
-    result = await task("https://wger.de/api/v2/exerciseinfo/?limit="+str(count))
+    try:
+        start = time()
+        count = await task()
+        count = count[0]['count']
+        counter = 0
+        result = await task("https://wger.de/api/v2/exerciseinfo/?limit="+str(count))
 
-    for x in result[0]['results']:
-        if x['category']['name'] == name: 
-            counter=counter+1
-            append(Exercises2(name= x['name'], 
-            description= x['description'], 
-            category=x['category']['name'], 
-            id_category=x['category']['id'] ,
-            muscles=x['muscles'],
-            equipment=x['equipment']).dict())
-   
-    result = append('', True)
-    if len(result) > 0 :
-        db.exercises.delete_many({"category": name})
-        db.exercises.insert_many(result)
-    return {"message": "Import OK","time": time() - start, "count": counter}
+        for x in result[0]['results']:
+            if x['category']['name'] == name: 
+                counter=counter+1
+                append(Exercises2(name= x['name'], 
+                description= x['description'], 
+                category=x['category']['name'], 
+                id_category=x['category']['id'] ,
+                muscles=x['muscles'],
+                equipment=x['equipment']).dict())
+    
+        result = append('', True)
+        if len(result) > 0 :
+            db.exercises.delete_many({"category": name})
+            db.exercises.insert_many(result)
+        logging.info('GET /import_category/' + name)
+        return {"message": "Import OK","time": time() - start, "count": counter}
+    except Exception as ex:
+        logging.exception(ex)
+        raise
+
 
 @app.put("/import_category2/{name}")
 async def import_category2(name: str, category: Category):
-    #print("ver {recuento} ")
-    if (recuento :=  db.data.find_one({"category": name})) is not None:
-        raise HTTPException(status_code=400, detail="La categoria " + name + " ya existe")
-    data = create_obj(name, category)
-    db.data.insert_one(data)
+    try:
+        #print("ver {recuento} ")
+        if (recuento :=  db.data.find_one({"category": name})) is not None:
+            raise HTTPException(status_code=400, detail="La categoria " + name + " ya existe")
+        data = create_obj(name, category)
+        db.data.insert_one(data)
+        logging.info('GET /import_category/' + name)
+        return {"name": name, "category": category}
+    except Exception as ex:
+        logging.exception(ex)
+        raise
 
-    return {"name": name, "category": category}
 
 @app.get("/get_routine/{days}")
 async def get_routine(days: int):
-    categorys = db.exercises.distinct('category')
-    categorys = random.sample( categorys, len(categorys) )
-    exercises_of_categorys = []
-    categorys_of_day = []
-
-    schedule = []
-    for x in range(days*3):
-
-        var = loads(dumps(db.exercises.find({"category": categorys[(x+1) % len(categorys)]}).limit(10)))
-        var = random.sample( var, len(var) )
-        for i in range(3) :
-            #exercises_of_categorys.append({"name": var[i]['name'], "description": var[i]['description'], "muscles": var[i]['muscles'], "equipment": var[i]['equipment'] })
-            exercises_of_categorys.append({"category": var[i]['category'], "name": var[i]['name'] })
-
-        categorys_of_day.append({"category": categorys[(x+1) % len(categorys)], "exercises": exercises_of_categorys})
+    try:
+        categorys = db.exercises.distinct('category')
+        categorys = random.sample( categorys, len(categorys) )
         exercises_of_categorys = []
+        categorys_of_day = []
 
-        if (x+1) % 3 == 0 :
-            #print((x+1) % len(categorys))
-            #print(categorys[(x+1) % len(categorys)])
-            #categorys_of_day.append({"category": categorys[(x+1) % len(categorys)], "exercises": exercises_of_categorys})
-            schedule.append({"day_of_week": math.ceil((x+1)/3),"training":  categorys_of_day})
-            categorys_of_day = []
-        #if (x+1) % 3 == 0 : schedule.append({"day_of_week": math.ceil((x+1)/3), categorys_of_day})
-    return {"date": datetime.datetime.now(), "schedule": schedule}
+        schedule = []
+        for x in range(days*3):
+
+            var = loads(dumps(db.exercises.find({"category": categorys[(x+1) % len(categorys)]}).limit(10)))
+            var = random.sample( var, len(var) )
+            for i in range(3) :
+                #exercises_of_categorys.append({"name": var[i]['name'], "description": var[i]['description'], "muscles": var[i]['muscles'], "equipment": var[i]['equipment'] })
+                exercises_of_categorys.append({"category": var[i]['category'], "name": var[i]['name'] })
+
+            categorys_of_day.append({"category": categorys[(x+1) % len(categorys)], "exercises": exercises_of_categorys})
+            exercises_of_categorys = []
+
+            if (x+1) % 3 == 0 :
+                #print((x+1) % len(categorys))
+                #print(categorys[(x+1) % len(categorys)])
+                #categorys_of_day.append({"category": categorys[(x+1) % len(categorys)], "exercises": exercises_of_categorys})
+                schedule.append({"day_of_week": math.ceil((x+1)/3),"training":  categorys_of_day})
+                categorys_of_day = []
+            #if (x+1) % 3 == 0 : schedule.append({"day_of_week": math.ceil((x+1)/3), categorys_of_day})
+        logging.info('GET /import_category/' + str(days))
+        return {"date": datetime.datetime.now(), "schedule": schedule}
+    except Exception as ex:
+        logging.exception(ex)
+        raise
